d896fcc0f10b1bb9376e67c56877ac8c67cdffdb
JWillemse <141319408+JacquesWillemse@users.noreply.github.com>
2026-04-08 14:03:09 +0200

    Job Trigger

--- Clinical Trial/Assets/Scripts/Hospital/MainSceneUIManager.cs
+++ Clinical Trial/Assets/Scripts/Hospital/MainSceneUIManager.cs
@@ -3,11 +3,17 @@ using UnityEngine.UI;
 
 public class MainSceneUIManager : MonoBehaviour
 {
-    public GameObject JobPanel;
 
+    //Job Panel UI
+    public GameObject JobPanel;
     public Button nurseButton;
     public Button doctorButton;
     public Button janitorButton;
+    public Button JobSelectorCloseButton;
+
+    //Information Panel UI
+    public Button InfoCloseButton;
+    public GameObject InfoPanel;
 
     public SimplePlayerMovement playerMovement;
     public JobSpawner jobSpawner;
@@ -18,34 +24,62 @@ public class MainSceneUIManager : MonoBehaviour
         nurseButton.onClick.AddListener(OnNurseClicked);
         doctorButton.onClick.AddListener(OnDoctorClicked);
         janitorButton.onClick.AddListener(OnJanitorClicked);
+        JobSelectorCloseButton.onClick.AddListener(OnJobCloseClicked);
+        InfoCloseButton.onClick.AddListener(OnInfoCloseClicked);
     }
+
     void OnNurseClicked()
     {
         Debug.Log("Nurse button clicked");
         jobSpawner.SpawnAtRandom(LocationType.Nurse);
-        playerMovement.SetControlsLocked(false);
-        JobPanel.SetActive(false);
     }
 
     void OnDoctorClicked()
     {
         Debug.Log("Doctor button clicked");
         jobSpawner.SpawnAtRandom(LocationType.Doctor);
-        playerMovement.SetControlsLocked(false);
-        JobPanel.SetActive(false);
+
     }
 
     void OnJanitorClicked()
     {
         Debug.Log("Janitor button clicked");
         jobSpawner.SpawnAtRandom(LocationType.Janitor);
+
+    }
+    void OnJobCloseClicked()
+    {
+        Debug.Log("Close button clicked");
         playerMovement.SetControlsLocked(false);
+        JobPanel.SetActive(false);
+    }
+    public void OnJobOpen()
+    {
+        Debug.Log("Open button clicked");
+        playerMovement.SetControlsLocked(true);
+        JobPanel.SetActive(true);
+    }
+
+    void OnInfoCloseClicked()
+    {
+        InfoPanel.SetActive(false);
+        playerMovement.SetControlsLocked(false);
+        JobPanel.SetActive(false);
+    }
+
+    void OnInfoOpenClicked()
+    {
+        InfoPanel.SetActive(true);
         JobPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            OnInfoOpenClicked();
+            playerMovement.SetControlsLocked(true);
+        }
     }
 }
--- Clinical Trial/Assets/Scripts/Hospital/MiniGameStart.cs
+++ Clinical Trial/Assets/Scripts/Hospital/MiniGameStart.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class MiniGameStart : MonoBehaviour
 {
     public string jobType = "Nurse";
+
+    public MainSceneUIManager mainSceneUIManager;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            if(jobType == "Manager")
+            {
+                mainSceneUIManager.OnJobOpen();
+                return;
+            }
             Debug.Log("transtion to " + jobType + " mini-game");
             Destroy(gameObject);
         }
